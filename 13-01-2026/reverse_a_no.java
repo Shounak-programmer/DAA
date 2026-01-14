1bfc08a7eb5388600e08dd8738dc5b43e908731e
Shounak <ShhounakChatterjee@gmail.com>
2026-01-14 20:11:17 +0530

    Add complexity analysis to reverse_a_no.java
    Added time and space complexity comments to the reverse number program.

--- 13-01-2026/reverse_a_no.java
+++ 13-01-2026/reverse_a_no.java
@@ -1,19 +1,20 @@
-import java.util.*;
-public class reverse_a_no {
-    public static void main(String args []) {
-        System.out.println("Enter a Number");
-        Scanner sc = new Scanner(System.in); 
-        int number = sc.nextInt();
-        int reversed = 0;
-        int temp = number;
+import java.util.*;      
+public class reverse_a_no {                               //1
+    public static void main(String args []) {             //1
+        System.out.println("Enter a Number");             //1
+        Scanner sc = new Scanner(System.in);              //1
+        int number = sc.nextInt();                        //1
+        int reversed = 0;                                 //1
+        int temp = number;                                //1
         
-        while (temp != 0) {
-            reversed = reversed * 10 + temp % 10;
-            temp /= 10;
-        }
+        while (temp != 0) {                               //log10(n) + 1 
+            reversed = reversed * 10 + temp % 10;         //log10(n)
+            temp /= 10;                                   //log10(n)
+        }                                                 //log10(n)
         
-        System.out.println("Original number: " + number);
-        System.out.println("Reversed number: " + reversed);
-        sc.close();
+        System.out.println("Original number: " + number);    //1
+        System.out.println("Reversed number: " + reversed);  //1
+        sc.close();                                        //1
     }
 }
+//Time Complexity = O(log10(n)), Space Complexity = O(1)
